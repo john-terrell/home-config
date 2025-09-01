cda1e4d682099cfc522349713cccea77eaff6e48
John W. Terrel <john@coolpeople.io>
2025-08-31 22:10:44 -0700

    WIP

--- modules/cli/neovim/default.nix
+++ modules/cli/neovim/default.nix
@@ -1,12 +1,13 @@
 {pkgs, ...}: {
+    home.packages = with pkgs; [
+        clang-tools
+    ];
+
     programs.neovim = {
         enable = true;
         defaultEditor = true;
         viAlias = true;
         vimAlias = true;
-        plugins = [
-            pkgs.vimPlugins.clang_complete
-        ];
         extraLuaConfig = ''
             require("johnt")
             '';
