53f8ccb0a79786c417397dc0e662ecae4e662ea2
John W. Terrell <john@coolpeople.io>
2025-09-06 22:33:04 -0700

    WIP

--- modules/cli/nixvim/default.nix
+++ modules/cli/nixvim/default.nix
@@ -1,23 +1,23 @@
 { self, pkgs, ... }:
 {
     programs.nixvim = {
-    imports = [ 
-        ./autocmd.nix
-        ./bufferline.nix 
-        ./diagnostic.nix
-        ./keymaps.nix
-        ./options.nix
-        ./plugins.nix
-    ];
+        imports = [
+            ./autocmd.nix
+            ./bufferline.nix
+            ./diagnostic.nix
+            ./keymaps.nix
+            ./options.nix
+            ./plugins.nix
+        ];
 
-    config = {
-    	enable = true;
-        colorschemes.catppuccin.enable = true;
-        globals = {
-            mapleader = " ";
+        config = {
+            enable = true;
+            colorschemes.catppuccin.enable = true;
+            globals = {
+                mapleader = " ";
+            };
+            vimAlias = true;
+            viAlias = true;
         };
-        vimAlias = true;
-        viAlias = true;
-    };
     };
 }
