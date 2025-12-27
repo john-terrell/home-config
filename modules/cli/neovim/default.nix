6af30b46d9295dd45d89034054304a8646434620
John W. Terrell <john@coolpeople.io>
2025-12-27 10:34:47 -0800

    Added additional packages to neovim

--- modules/cli/neovim/default.nix
+++ modules/cli/neovim/default.nix
@@ -1,20 +1,28 @@
-{pkgs, lib, ...}: {
-    programs.neovim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
+{ pkgs, lib, ... }:
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
 
-        extraPackages = with pkgs; [
-            clang-tools
-            clang
-            cmake
-            tree-sitter
-            vscode-extensions.vadimcn.vscode-lldb
-        ];
+    extraPackages = with pkgs; [
+      chafa
+      clang-tools
+      clang
+      cmake
+      fishMinimal
+      nixfmt-rfc-style
+      packer
+      python3
+      rust-analyzer
+      tree-sitter
+      ueberzugpp
+      viu
+      vscode-extensions.vadimcn.vscode-lldb
+      wget
+    ];
 
-        plugins = with pkgs.vimPlugins; [
-            nvim-treesitter.withAllGrammars
-        ];
-    };
+    plugins = with pkgs.vimPlugins; [ nvim-treesitter.withAllGrammars ];
+  };
 }
